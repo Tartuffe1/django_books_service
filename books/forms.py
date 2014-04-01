5070b65edcd4b628a3ec62738e16f30bebbb10e5
Dennis Zellich <dzelic@email.ht.hr>
2014-04-01 16:48:56 +0200

    Upload files improved - adding pictures

--- books/forms.py
+++ books/forms.py
@@ -4,5 +4,4 @@ from models import Book
 class BookForm(forms.ModelForm):
    class Meta:
       model=Book
-      fields= ('title', 'author', 'publisher', 'year', 'binding', 'pages', 'soundness','category','price', 'extra', 'docfile')
-      
+      fields= ('title', 'author', 'category', 'price', 'docfile')
--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from datetime import datetime 
 
+# Create your models here.
 class Book(models.Model):
     CATEGORY_CHOICES = (
         ('expert', 'Expert books'),
@@ -12,17 +12,9 @@ class Book(models.Model):
     
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publisher = models.CharField(max_length=200)
-    year = models.IntegerField(default=1)
-    binding = models.CharField(max_length=10)
-    pages = models.IntegerField(default=1)
-    soundness = models.CharField(max_length=50)
     category = models.CharField(max_length=15, choices = CATEGORY_CHOICES)
-    price = models.IntegerField(default=1)
-    extra = models.CharField(max_length=200)
-    date = models.DateField(default=datetime.now())
-    docfile = models.FileField(upload_to='images/%Y/%m/%d')
-    user_id=models.IntegerField(default=1)
+    price= models.IntegerField()
+    docfile = models.FileField(upload_to='images/%Y/%m/%d', default='images/default.jpg')
     
     def __unicode__(self):
         return self.title
